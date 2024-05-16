1c2e90d5ed3a25f0329056e4ddba87d09e4e308b
pes18fan <pes18fan@disr.it>
2024-05-16 18:43:42 +0545

    fix: fix indentation

--- web/check_primes.php
+++ web/check_primes.php
@@ -1,15 +1,15 @@
 <?php
     function checkPrime($num) {
-	$c = 0;
+        $c = 0;
 
-	if ($num == 1) echo "The number is 1, which is neither prime nor composite";
+        if ($num == 1) echo "The number is 1, which is neither prime nor composite";
 
-	for ($i = 1; $i <= $num; $i++) {
-	    if ($num % $i == 0) $c++;
-	}
+        for ($i = 1; $i <= $num; $i++) {
+            if ($num % $i == 0) $c++;
+        }
 
-	if ($c == 2) echo "The number $num is prime.";
-	else echo "The number $num is composite.";
+        if ($c == 2) echo "The number $num is prime.";
+        else echo "The number $num is composite.";
     }
 
     $n = 69;
