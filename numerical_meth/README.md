af83c245022a1d8f3be6c67a4907f674b43e582c
pes18fan <pes18fan@disr.it>
2024-12-05 18:11:38 +0545

    feat: change the numerical method programs

--- numerical_meth/README.md
+++ numerical_meth/README.md
@@ -4,9 +4,3 @@ you need to link the math library from `libc` using this command:
 ```bash
 gcc program.c -o output -lm
 ```
-
-Additionally, for user input like the function to approximate the result of and
-initial guesses, a correspoding header file is provided where you can input
-those values. Initial guesses are likely to be computable though, so in the future
-I might change the programs to compute an initial guess by themselves; such that
-the only thing left to input is the function to approximate.
---
+++ numerical_meth/bisection.c
@@ -0,0 +1,63 @@
+#include <math.h>
+#include <stdio.h>
+#define E 0.001
+
+double f(double x) { return x * x - 4 * x - 10; }
+
+double g(double x) { return (4 * sin(x)) - exp(x); }
+
+double approximator(double (*f)(double x)) {
+    double x1, x2, f1, f2;
+    double x0 = -1;
+    double f0 = -1;
+
+    for (;;) {
+        printf("Enter x1 and x2: \n");
+        scanf("%lf %lf", &x1, &x2);
+
+        f1 = f(x1);
+        f2 = f(x2);
+
+        if (f1 * f2 >= 0) {
+            printf("Product of f(x1) and f(x2) must be negative but isn't."
+                   "Try other values.\n");
+        } else {
+            break;
+        }
+    }
+
+    printf("\n\n");
+    printf("S.N\tx1\t\tx2\t\tx0\t\tf1\t\tf2\t\tf0\n");
+
+    for (int count = 1;; count++) {
+        f1 = f(x1);
+        f2 = f(x2);
+
+        x0 = (x1 + x2) / 2;
+        f0 = f(x0);
+
+        printf("%d\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n", count, x1, x2, x0, f1, f2,
+               f0);
+
+        if (f0 == 0) {
+            printf("\n");
+            break;
+        } else if (f1 * f0 > 0) {
+            x1 = x0;
+        } else {
+            x2 = x0;
+        }
+
+        if (fabs(x2 - x1) <= E) {
+            printf("\n");
+            break;
+        }
+    }
+
+    return x0;
+}
+
+int main() {
+    printf("For f(x), approximation is %lf\n\n", approximator(f));
+    printf("For g(x), approximation is %lf", approximator(g));
+}
