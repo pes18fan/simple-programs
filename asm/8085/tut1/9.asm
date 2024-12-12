9d5fce81c266dcac1590a4639c78065b107734bf
pes18fan <pes18fan@disr.it>
2024-12-12 19:17:43 +0545

    fix: another typo, in 9.asm

--- asm/8085/tut1/9.asm
+++ asm/8085/tut1/9.asm
@@ -1,7 +1,7 @@
 ; If the content of 2050h is greater than or equal to 64h, display 0Fh
 ; Else display FFh
 
-ldax 2050h      ; a <- [2050]
+lda 2050h      ; a <- [2050]
 cpi 64h         ; a - 64
 jc fallback
 mvi a, 0Fh      ; a <- 0F
