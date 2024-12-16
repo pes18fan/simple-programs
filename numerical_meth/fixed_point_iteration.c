b9bba422ec07f402b07917b87e404734ea44f4d4
pes18fan <pes18fan@disr.it>
2024-12-16 18:45:22 +0545

    feat: add more qns in numerical method

--- numerical_meth/fixed_point_iteration.c
+++ numerical_meth/fixed_point_iteration.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #define E 0.001
 
-double f(double x) { return x * x - x - 2; }
+double f(double x) { return x * x + x - 2; }
 
 /* phi is recieved by simplifying f(x) = 0 to create x = phi(x). There are
  * often multiple possible phi functions, and the condition for one to be
@@ -12,7 +12,11 @@ double f(double x) { return x * x - x - 2; }
  * After phi(x) is found, finding the root is exceedingly simple; just iterate
  * with it again and again to improve the approximation on each one of them.
  */
-double phi(double x) { return 2 / (x - 1); }
+double phi(double x) { return 2 - x * x; }
+
+double g(double x) { return x * x - 5; }
+
+double phi_2(double x) { return cbrt(5 * x); }
 
 double approximator(double (*f)(double x), double (*phi)(double x)) {
     double x1, x0;
@@ -22,7 +26,7 @@ double approximator(double (*f)(double x), double (*phi)(double x)) {
 
     printf("\n");
 
-    printf("S.N\tx0\tx1\n");
+    printf("S.N\tx0\t\tx1\n");
 
     for (int count = 1;; count++) {
         x1 = phi(x0);
@@ -40,4 +44,7 @@ double approximator(double (*f)(double x), double (*phi)(double x)) {
     return x1;
 }
 
-int main() { printf("Approximation is %lf", approximator(f, phi)); }
+int main() {
+    printf("Approximation for 1a is %lf\n", approximator(f, phi));
+    printf("Approximation for 1b is %lf", approximator(g, phi_2));
+}
--- numerical_meth/newton_raphson.c
+++ numerical_meth/newton_raphson.c
@@ -7,6 +7,12 @@ double f(double x) { return x * x - 4 * x - 10; }
 /* The derivative of f(x), necessary for this method. */
 double f_deriv(double x) { return 2 * x - 4; }
 
+double g(double x) { return x * tan(x) - 1; }
+
+double sec(double x) { return 1 / (tan(x)); }
+
+double g_deriv(double x) { return x * pow(sec(x), 2) + tan(x); }
+
 double approximator(double (*f)(double x), double (*f_deriv)(double x)) {
     double x0, x1, f0, f0_deriv;
 
@@ -19,6 +25,11 @@ double approximator(double (*f)(double x), double (*f_deriv)(double x)) {
     for (int count = 1;; count++) {
         f0 = f(x0);
         f0_deriv = f_deriv(x0);
+
+        if (f0_deriv == 0) {
+            printf("f0_deriv was found to be zero. Method failed.\n");
+            return -1;
+        }
 
         x1 = x0 - (f0 / f0_deriv); // The Newton-Raphson formula
 
@@ -39,5 +50,7 @@ double approximator(double (*f)(double x), double (*f_deriv)(double x)) {
 }
 
 int main() {
-    printf("Approximation of the root is %lf", approximator(f, f_deriv));
+    printf("Approximation of the root of 1a is %lf\n",
+           approximator(f, f_deriv));
+    printf("Approximation of the root of 1b is %lf", approximator(g, g_deriv));
 }
